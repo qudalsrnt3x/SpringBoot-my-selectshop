bea0e78959ebd1ecffeb7f3e7328640a7cbd4ab5
qudalsrnt3x <qudalsrnt3x@gmail.com>
2021-09-16 10:38:17 +0900

    Initial commit

---
+++ src/main/java/com/sparta/myselectshop/MySelectshopApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myselectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySelectshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySelectshopApplication.class, args);
+    }
+
+}
